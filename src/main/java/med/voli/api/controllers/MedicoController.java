97f77a9129c23e38861621599445c7f853264cf4
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-06-15 14:55:57 -0500

    creando dto para listar los datos de medico que se requieren

--- src/main/java/med/voli/api/controllers/MedicoController.java
+++ src/main/java/med/voli/api/controllers/MedicoController.java
@@ -1,13 +1,16 @@
 package med.voli.api.controllers;
 import jakarta.validation.Valid;
+import med.voli.api.medico.DatosListadoMedico;
 import med.voli.api.medico.DatosRegistroMedico;
 import med.voli.api.medico.Medico;
 import med.voli.api.repository.iMedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/medicos")
@@ -19,6 +22,16 @@ public class MedicoController {
     @PostMapping
     public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
         medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+
+//    @GetMapping
+//    public List<DatosListadoMedico> listadoMedicos(){
+//        return medicoRepository.findAll().stream().map(DatosListadoMedico::new).toList();
+//    }
+
+    @GetMapping
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable pageable){
+        return medicoRepository.findAll(pageable).map(DatosListadoMedico::new);
     }
 
 }
