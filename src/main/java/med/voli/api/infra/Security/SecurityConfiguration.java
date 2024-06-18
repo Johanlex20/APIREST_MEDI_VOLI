68aeb461896a0b9aa20b3b981cc5ba024e03ac08
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-06-18 12:19:25 -0500

    reformamos la configuracion desguridad, fortalezemos los filtros, modificamos el token

--- src/main/java/med/voli/api/infra/Security/SecurityConfiguration.java
+++ src/main/java/med/voli/api/infra/Security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package med.voli.api.infra.Security;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +12,29 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        a -> a
+                                .requestMatchers(HttpMethod.POST, "/login")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).addFilterBefore(securityFilter , UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(h -> h.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
--- src/main/java/med/voli/api/infra/Security/SecurityFilter.java
+++ src/main/java/med/voli/api/infra/Security/SecurityFilter.java
@@ -3,6 +3,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voli.api.repository.iUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -10,11 +14,28 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private iUsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //OBTENER TOKEN DEL HEADER
-        var token = request.getHeader("Authorization").replace("Bearer ", "");
-        System.out.println(token);
+        var authHeader = request.getHeader("Authorization");
+        System.out.println(authHeader);
+        if (authHeader != null){
+            var token = authHeader.replace("Bearer ", "");
+            var nombreUsuario = tokenService.getSubject(token);
+            if (nombreUsuario != null){
+                // TOKEN VALIDO
+                var usuario = usuarioRepository.findByLogin(nombreUsuario);
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
         filterChain.doFilter(request,response);
+
     }
 }
